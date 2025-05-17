7223c83e8e79549b151651ae87842b3c34097cba
Sumedha Parande <sumedhapparande@gmail.com>
2025-05-17 00:24:42 -0400

    Fix asset loading and remove path prefix

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/sumedha-parande-portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/sumedha-parande-portfolio/' : '',
+  basePath: '',
+  assetPrefix: '',
 }
 
 module.exports = nextConfig 
